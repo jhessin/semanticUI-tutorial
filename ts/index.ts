f49f36afbc55d294024afea52485a54faf3dd997
Jim Hessin <jhessin@gmail.com>
2021-05-18 18:33:32 -0600

    Scrubbed to get ready for the project

--- ts/index.ts
+++ ts/index.ts
@@ -1,15 +1,2 @@
 /** @format */
 import $ from 'jquery';
-
-function pressButton() {
-  const input = $('#input-text').val() as string;
-  const output = $('#output-text');
-
-  output.html(`
-    <h1>${input}</h1>
-  `);
-}
-
-const button = $('#submit-button');
-
-button.on('click', pressButton);
